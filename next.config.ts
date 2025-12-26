d6d01ea60ef8775f7916242afe9048468f1c0817
Xelvora <99201657+dindazahrars@users.noreply.github.com>
2025-12-26 11:06:37 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,33 +6,12 @@ const nextConfig: NextConfig = {
   },
 
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-        port: "",
-        pathname: "/**",
-      },
-
-      // ⬇️ domain gambar Nusantara
-      {
-        protocol: "https",
-        hostname: "img-global.cpcdn.com",
-      },
-      {
-        protocol: "https",
-        hostname: "akcdn.detik.net.id",
-      },
-      {
-        protocol: "https",
-        hostname: "images.pexels.com",
-      },
+    domains: [
+      "images.unsplash.com",
+      "lh3.googleusercontent.com",
+      "img-global.cpcdn.com",
+      "akcdn.detik.net.id",
+      "images.pexels.com",
     ],
   },
 };
